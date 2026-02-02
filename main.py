e513c1f8eba55a3fca44d7ac0b08e9083832c2ce
ncread <noahcread@gmail.com>
2026-02-01 21:23:53 -0800

    update data types for lru_cache implementation

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from flask import Flask, request, render_template, jsonify
 import time
 from get_data import *
 
+
 app = Flask(__name__)
+
 
 @app.route('/healthz', methods = ['GET'])
 def site_health_check():
     return jsonify({'status':'healthy'}), 200
 
+
 @app.route('/', methods = ['GET','POST'])
 def index():
     corr_chart_html = ''
     tickers = ''
-    hist_ticker = ''
     hist_fig_html = ''
     metrics = {}
     time_period = '6mo'
-    # time_series_html = ''
     time_series_list = []
 
     if request.method == 'POST':
@@ -27,20 +28,18 @@ def index():
 
             ticker_list = [t.strip().upper() for t in tickers.split(",") if t.strip()]
 
-            ###call a function here that outputs a dataframe that the other functions
-            #can pull from to avoid using yf numerous times
-
-            for _ in range(3):
+            for _ in range(2):
                 try:
-                    df = get_historical_data(ticker_list, time_period)
+                    ticker_tuple = tuple(ticker_list) #necessary for lru_cache
+                    df = get_historical_data(ticker_tuple, time_period)
 
-                    metrics = get_metrics(ticker_list)
+                    metrics = get_metrics(ticker_tuple)
 
-                    corr_chart_html = generate_corr_plot(df, ticker_list, time_period)
-                    
+                    corr_chart_html = generate_corr_plot(df, time_period)
                     
                     for stock in ticker_list:
                         time_series_list.append(get_time_series(df, stock, time_period))
+                    time.sleep(2)
                     break
                 except Exception as e:
                     # corr_chart_html = f'<p>Error fetching data from Yahoo Finance. Please try again.</p>'
@@ -54,4 +53,4 @@ def index():
                            time_series_list=time_series_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
